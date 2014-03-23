4ecd9bbc65c8fcd0a6d6b8e1e07cb0b4e976b3dc
Dusty Jewett <dusty.jewett@gmail.com>
2014-03-23 12:46:06 -0700

    Removing last vestiges of Mongo

--- vagrantBootstrap.sh
+++ vagrantBootstrap.sh
@@ -18,8 +18,6 @@ then
     # Required for apt-add-repository
     sudo apt-get install -y python-software-properties
     sudo apt-add-repository -y ppa:chris-lea/node.js
-    sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-    echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/10gen.list
     sudo apt-get update
     touch /var/log/1aptsetup
 fi
