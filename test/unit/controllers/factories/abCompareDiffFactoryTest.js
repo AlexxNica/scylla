f73dc8c9b97e843fb7c6ff9d78964cbc91394f75
Dusty Jewett <dusty.jewett@gmail.com>
2014-05-04 18:13:55 -0700

    Removing commented code.

--- test/unit/controllers/factories/abCompareDiffFactoryTest.js
+++ test/unit/controllers/factories/abCompareDiffFactoryTest.js
@@ -25,43 +25,7 @@ describe('AB Compare Factory', function(){
         });
     });
 
-    /*
 
-
-    var webPageToSnapshotSuccessResponse = {
-        console: 'Opening Page: http://127.0.0.1:3000/testFodder/simpleChanges.html?phantomjs\n',
-        message: '',
-        state: 'Captured',
-        image:{
-            contents: '',
-            info: {
-                Image: ' /tmp/charybdis-ca-114229-10852-r55l8j.png',
-                properties: {}
-            }
-        }
-    };
-    var imagesCreateSuccessResponse =  {
-        width: 800,
-        height: 800,
-        url: '1/snapshot-2014329-1drwvtx.png',
-        id: 1,
-        updatedAt: new Date(),
-        createdAt: new Date(),
-        setSnapshot:sinon.spy(),
-        save:sinon.stub.returns(Q.resolve({}))
-    };
-    var sharedBuildAndValidateModelSuccessResponse = {
-        console: 'Opening Page: http://127.0.0.1:3000/testFodder/simpleChanges.html?phantomjs\n',
-        message: '',
-        state: 'Complete',
-        id: 3,
-        updatedAt: new Date(),
-        createdAt: new Date(),
-        setPage:sinon.spy(),
-        setImage:sinon.spy(),
-        save:sinon.stub().returns(Q.resolve({}))
-    };
-    */
     var abComparesFindByIdSuccessResponse = {
         pageAId:2,
         pageBId:3
